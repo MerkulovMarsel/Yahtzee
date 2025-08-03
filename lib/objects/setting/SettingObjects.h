4d60fa21c66f3ed09bebd83c35626cdaed180f53
MerkulovMarsel <144001011+MerkulovMarsel@users.noreply.github.com>
2025-08-03 13:36:55 +0300

    add some

--- lib/objects/setting/SettingObjects.h
+++ lib/objects/setting/SettingObjects.h
@@ -4,11 +4,15 @@
 
 #ifndef SETTINGOBJECTS_H
 #define SETTINGOBJECTS_H
-
+#include <cstdint>
 
 
 class SettingObjects {
-
+    enum class SettingButtonsType : std::uint8_t {
+        CLASSIC_MODE,
+        SPEED_MODE,
+        COUNTDOWN_MODE
+    };
 };
 
 
